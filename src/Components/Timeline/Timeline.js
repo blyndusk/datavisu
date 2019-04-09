e983e7d8a5baf3114f27cdadd6d5e6858b23cdef
blyndusk <alexandredelaloydev@gmail.com>
2019-04-09 21:23:40 +0200

    ✨ fix timeline generation

--- src/Components/Timeline/Timeline.js
+++ src/Components/Timeline/Timeline.js
@@ -6,14 +6,15 @@ class Timeline extends Component {
         this.state = {
             timeline: []
         }
-        this.xpos = 0;
+        // total length of the timelinex
+        this.yearPosY = 0;
     }
 
     componentDidMount = () => {
         this.fetchData()
     }
     fetchData = () => {
-        fetch('https://gist.githubusercontent.com/blyndusk/d789375e1a6309f82745bcfa3477f64f/raw/394da74bb7244563b84017a0c9fba2f231a7b22c/timeline.json')
+        fetch('https://gist.githubusercontent.com/blyndusk/d789375e1a6309f82745bcfa3477f64f/raw/2bfea3519938d430b8f922b064c2b54567ea87db/timeline.json')
         .then((fetched) => {
             return fetched.json();
         })
@@ -21,16 +22,64 @@ class Timeline extends Component {
             this.setState({timeline: json})
         }); 
     }
-    getPrizesLength = (x, i) => this.state.timeline.map((x, i) => {
-            let lgh = 0
+    getPrizesLength = () => this.state.timeline.map((x, i) => {
+            // every year, yearPosY is incremented by 20
+            this.yearPosY += 20;
+            // total length of poeple in a specific year
+            let totalPeopleLength = 0;
+            // rendered array, which'll contain people in a category
+            const SVGcategoryGroup = [];
+            // rendered array, which'll contain a person for a nobel prize
+            const SVGPeopleGroup = [];
+            // SVG circles position
+            let circlePos= {
+                x: this.yearPosY,
+                y: -10
+            };
+            // for all categories in a year
             for (let j = 0; j < x.categories.length; j++) {
-                const y = x.categories[j];
-                lgh += y.length;
+                // a category
+                const category = x.categories[j];
+                // increment the total length with each length
+                totalPeopleLength += category.length;
+                // update the circle y postion by one unit
+                circlePos.y += 10;
+                // init a subgroup array to staock all people
+                const SVGPeopleSubGroup = [];
+                // for every people in a category
+                for (let k = 0; k < category.length; k++) {
+                    // a person
+                    const person = category[k].name;
+                    // update the circle y postion by one unit
+                    circlePos.y += 10;
+                    // push a circle for each person
+                    SVGPeopleSubGroup.push((<circle 
+                        key={`${i}${j}${k}`}
+                        data-label={j}
+                        r="5" 
+                        cx={circlePos.x}
+                        cy={200 - circlePos.y}
+                    >{person}</circle>));
+                }
+                // group all person in people
+                SVGPeopleGroup.push(<g key={`${i}${j}`} className="peoples">{SVGPeopleSubGroup}</g>)
             }
-            this.xpos+=20
-            console.log(lgh)
-            return <line key={i} x1={this.xpos} y1={200 - lgh * 10} x2={this.xpos} y2={200}/>
+            // group all people in a category
+            SVGcategoryGroup.push(<g key={i} className="category">{SVGPeopleGroup}</g>)
+            return (
+                // group all categories in a year
+                <g key={i} className="year">
+                    {/* the line */}
+                    <line  x1={this.yearPosY }  y1={200 - totalPeopleLength * 20} x2={this.yearPosY }  y2={200} onMouseEnter={() => this.mouseup(x.year)}/>
+                    {/* circles */}
+                    {SVGcategoryGroup}
+                </g>
+            )
         })
+    mouseup = (year) => {
+        console.log('yeyey')
+        return <p>{year}</p>
+    } 
     render() {
         return (
         <div className="Timeline">
