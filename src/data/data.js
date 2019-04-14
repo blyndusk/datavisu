987ba97a00354c63259bda5f1f24c8ee0dfb5850
blyndusk <alexandredelaloydev@gmail.com>
2019-04-14 16:28:50 +0200

    ♻️ update data generator

--- src/data/data.js
+++ src/data/data.js
@@ -1,40 +1,32 @@
 const data = [];
-
-function rdmInt(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-}
+const rdmInt = (min, max) => Math.floor(Math.random() * (max - min) + min)
 
 export const generateData = () => {
-    const years = rdmInt(1, 5)
-    for (let i = 0; i < years; i++) {
-        data.push({
-            "year": rdmInt(1950, 2019),
-            "prizeList": []
-        })
-    }
-    for (let i = 0; i < data.length; i++) {
-        const prizeListInt = rdmInt(0, 6);
-        for (let k = 0; k < prizeListInt; k++) {
-            data[i].prizeList.push([])
-        }
-    }
-    for (let i = 0; i < data.length; i++) {
-        const item = data[i].prizeList
-        for (let j = 0; j < item.length; j++) {
-            const item2 = item[j];
-            for (let k = 0; k < rdmInt(0, 5); k++) {
-                item2.push({
-                    "name":"jan",
-                    "bio":"i'm a bio",
-                    "data": {
-                        "field": j,
-                        "country": "FR",
-                        "gender": Math.round(Math.random()),
-                        "age": rdmInt(22, 70)
-                    }
-                })
+    const years = rdmInt(5, 10);
+    let year = 1934
+    // years generation
+    for (let i = 0; i < years; i++) data.push({
+        "year": year++,
+        "prizeList": []
+    });
+    // categories generation
+    data.map(x => {
+        for (let k = 0; k < rdmInt(0, 6); k++) x.prizeList.push([]);
+        return console.clear();
+    })
+    // price winners generation
+    data.map(x => x.prizeList.map((y, j) => {
+        for (let k = 0; k < rdmInt(0, 5); k++) y.push({
+            "name":"jan",
+            "bio":"i'm a bio",
+            "data": {
+                "field": j,
+                "country": "FR",
+                "gender": Math.round(Math.random()),
+                "age": rdmInt(22, 70)
             }
-        }
-    }
+        });
+        return console.clear();
+    }));
     return data
 }
