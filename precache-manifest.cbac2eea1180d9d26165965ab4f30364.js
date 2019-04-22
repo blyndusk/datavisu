d567674e1f67ac6ad4bb144635ade57ff7ee55ea
blyndusk <alexandredelaloydev@gmail.com>
2019-04-22 20:02:50 +0200

    Updates

---
+++ precache-manifest.cbac2eea1180d9d26165965ab4f30364.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6fc48ec6f22194b36fcd",
+    "url": "/datavisu/static/css/main.2a00117d.chunk.css"
+  },
+  {
+    "revision": "6fc48ec6f22194b36fcd",
+    "url": "/datavisu/static/js/main.ad9cf1e6.chunk.js"
+  },
+  {
+    "revision": "4187ab8c8fd2753675cf",
+    "url": "/datavisu/static/js/runtime~main.cf015f01.js"
+  },
+  {
+    "revision": "ef06ab93e7747b591c34",
+    "url": "/datavisu/static/js/2.f275399c.chunk.js"
+  },
+  {
+    "revision": "05a001777538ada5def1dcb56f02367d",
+    "url": "/datavisu/index.html"
+  }
+];
