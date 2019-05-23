a68f40e412741bc0b3e6877a3582889bd3aeaca4
blyndusk <alexandredelaloydev@gmail.com>
2019-05-23 21:21:11 +0200

    Updates

---
+++ precache-manifest.3070f055965a22c8ae0f8e5bef4983f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "06793a7d044a5b5b2c2c",
+    "url": "/unknown-genius/static/css/main.e9d9a986.chunk.css"
+  },
+  {
+    "revision": "06793a7d044a5b5b2c2c",
+    "url": "/unknown-genius/static/js/main.48ebb60d.chunk.js"
+  },
+  {
+    "revision": "24dd6f99b93b09926d9c",
+    "url": "/unknown-genius/static/js/runtime~main.415f7bfa.js"
+  },
+  {
+    "revision": "c918bb086e88fe0bc4f9",
+    "url": "/unknown-genius/static/js/2.7ff2c12e.chunk.js"
+  },
+  {
+    "revision": "22128e3340bb3dba305843197cab0b98",
+    "url": "/unknown-genius/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/unknown-genius/precache-manifest.d96a5a6deb592579878b3ceae2f15310.js"
+  "/unknown-genius/precache-manifest.3070f055965a22c8ae0f8e5bef4983f3.js"
 );
 
 workbox.clientsClaim();
