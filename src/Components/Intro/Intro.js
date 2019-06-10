b56e80ba1a340c1b550f3779484756cc1500b999
Maxime CHARPENTIER <maxime.charpentier@macbook-pro-de-admin.home>
2019-06-10 19:15:30 +0200

    Add Intro component's style / Add fonts / Change SCSS variables

--- src/Components/Intro/Intro.js
+++ src/Components/Intro/Intro.js
@@ -4,7 +4,11 @@ import React, { Component } from 'react';
 class Intro extends Component {
     render() {
         return <section className="Intro">
-            <h2>Intro</h2>
+            <div className="Intro__container">
+                <h2 className="Intro__title">Nobel Prize</h2>
+                <h3 className="Intro__subtitle">An interactive data-visualization on Nobel Prizes</h3>
+                <button className="Intro__button">Start</button>
+            </div>
         </section>
     }
 }
