4289258ae5cfbcac1a476c3d507b58305719735b
Maxime CHARPENTIER <maxime.charpentier@macbook-pro-de-admin.home>
2019-06-11 18:54:16 +0200

    Add Nav & Compare components (style & structure) / Add Home style

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,17 +1,18 @@
-
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import Nav from './../Nav/Nav'
+import Compare from './../Compare/Compare'
 
 class Home extends Component {
     
     render() {
-        return <section className="Home">   
-            <h2>Home</h2>
-            <nav id="nav">
-                <Link to="/map">MAP</Link>
-                <Link to="/timeline">TL</Link>
-            </nav>
-            <button id="comparator"><Link to="/comparator">COMPARE COUNTRIES</Link></button>
+        return <section className="Home">
+            <div className="Home__container">
+                <h3 className="Home__title">Explore by</h3>
+                <Nav />
+            </div>
+            <Compare />
         </section>
     }
 }
