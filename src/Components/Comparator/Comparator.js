88a7101935f78524d50a30e2ca7e1cee4c3a5d7c
blyndusk <alexandredelaloydev@gmail.com>
2019-06-11 20:51:06 +0200

    :ok_hand: fix parity

--- src/Components/Comparator/Comparator.js
+++ src/Components/Comparator/Comparator.js
@@ -11,10 +11,31 @@ class Comparator extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            codes: {
+                first: "FR", 
+                second: "US"
+            },
             firstCountryCode: "FR",
             secondCountryCode: "US",
             firstCountryData: [],
             secondCountryData: [],
+            countries: {
+                total: 0,
+                first: {
+                    parity: {
+                        f: [],
+                        m: [],
+                        total: 0
+                    }
+                },
+                second: {
+                    parity: {
+                        f: [],
+                        m: [],
+                        total: 0
+                    }
+                }
+            },
             baseUrl: 'http://localhost:8000/api/',
             type: [
                 'people',
@@ -23,13 +44,18 @@ class Comparator extends Component {
             params: {
                 field: 'idprice.idcategory.category',
                 country: 'idcountry.code',
-                peopleCountry: 'idpeople.idcountry.code'
+                peopleCountry: 'idpeople.idcountry.code',
+                gender: 'gender'
             },
         }
     }
     componentDidMount = () => {
         this.getData(this.state.firstCountryCode, 'firstCountryData');
         this.getData(this.state.secondCountryCode, 'secondCountryData');
+        this.genderCall(this.state.codes.first, 'first', 'm')
+        this.genderCall(this.state.codes.first, 'first', 'f')
+        this.genderCall(this.state.codes.second, 'second', 'm')
+        this.genderCall(this.state.codes.second, 'second', 'f')
     }
     getData = (countryCode, countryData) => {
         let params = {}
@@ -42,22 +68,56 @@ class Comparator extends Component {
             this.setState({
                 ...newState
             })
+            
         })
         .catch(err => console.log(err))
     }
+    genderCall = (countryCode, index, genderCode) => {
+        // param is empty by default
+        let params = {}
+        // add country param
+        params[this.state.params.country] = countryCode;
+        params.gender = genderCode;
+        // console.log(params)
+        // axios call with custom params
+        axios.get(this.state.baseUrl + this.state.type[0], { params })
+        // then, update #dev status
+        .then(res => {
+            console.log(params)
+            // arayy of people (M/F) of a country
+            const response = res.data["hydra:member"];
+                // update data state, conserving previous state
+                this.setState(prevState => {
+                    let countries = Object.assign({}, prevState.countries);
+                    countries[index].parity[genderCode] = response;
+                    countries[index].parity.total += response.length;
+                    countries.total += response.length
+                    return { countries };
+                }, () => console.log(this.state.countries))
+            }
+        )
+        .catch(err => console.log(err))
+        
+    }
+    
     render() {
         return <section className="Comparator">   
-            <h2>Comparator</h2>
-            <CompareAge/>
+            
             <CompareFields
                 firstCountryData={this.state.firstCountryData}
                 secondCountryData={this.state.secondCountryData}
             />
             <CompareParity
+                countries={this.state.countries}
+            />
+            <CompareAge
+                firstCountryData={this.state.firstCountryGenderData}
+                secondCountryData={this.state.secondCountryGenderData}
+            />
+            <CompareUniversities
                 firstCountryData={this.state.firstCountryData}
                 secondCountryData={this.state.secondCountryData}
             />
-            <CompareUniversities/>
         </section>
     }
 }
--- src/Components/Comparator/CompareParity/CompareParity.js
+++ src/Components/Comparator/CompareParity/CompareParity.js
@@ -9,7 +9,6 @@ class CompareParity extends Component {
             svgStyle: {},
             pathStyle: {},
             firstCountry: {
-                name: '',
                 parity: {
                     m: {
                         amount: 0,
@@ -22,7 +21,6 @@ class CompareParity extends Component {
                 },
             },
             secondCountry: {
-                name: '',
                 parity: {
                     m: {
                         amount: 0,
@@ -36,19 +34,55 @@ class CompareParity extends Component {
             }
         }
     }
+    componentWillReceiveProps() {
+        // this.setState({ 
+        //     firstCountry: this.props.firstCountryData
+        // })
+        // const parity = {
+        //     m: {
+        //         amount: this.props.firstCountryData.parity.m.length,
+        //         percent: 0
+        //     },
+        //     f: {
+        //         amount: 0,
+        //         percent: 0
+        //     }
+        // }
+        // console.log(this.props.firstCountryData.parity.m.length)
+        // this.setState({
+        //     firstCountry: {
+        //         parity
+        //     }
+        // })
+        this.getParity()
+    }
     componentDidUpdate = (prevProps) => {
-        if (this.props.firstCountryData !== prevProps.firstCountryData ) {
-            this.setState({firstCountry: {
-                name: this.props.firstCountryData[0].idcountry.name
-            }})
-            this.setCountry(this.props.firstCountryData, 0)
-            
+
+        if ( this.props.countries.total !==  prevProps.countries.total ) {
+            console.log('defe')
+             const setPercent = (gender, data) => Math.floor((gender.amount / data.length) * 100);
+            const parity = {
+                    m: {
+                        amount: this.props.countries.first.parity.m.length,
+                        percent:  Math.floor((this.props.countries.first.parity.m.length / this.props.countries.first.parity.total) * 100)
+                    },
+                    f: {
+                        amount: this.props.countries.first.parity.f.length,
+                        percent: Math.floor((this.props.countries.first.parity.f.length / this.props.countries.first.parity.total) * 100)
+                    }
+                }
+            this.setState({
+                firstCountry: {
+                    parity
+                }
+            })
         }
         if (this.props.secondCountryData !== prevProps.secondCountryData) {
-            this.setState({secondCountry: {
-                name: this.props.secondCountryData[0].idcountry.name
-            }})
-            this.setCountry(this.props.secondCountryData, 1)
+
+            // this.setState({secondCountry: {
+            //     name: this.props.secondCountryData[0].idcountry.name
+            // }})
+            // this.setCountry(this.props.secondCountryData, 1)
         }
     }
     setName = (countrybin) => {
@@ -63,42 +97,52 @@ class CompareParity extends Component {
         this.getParity(data, countrybin);
     }
     getParity = (data, countrybin) => {
+        // console.log(this.props.firstCountryData.dude)
         // parity content amount of men & women, and percentage
-        const parity = {
-            m: {
-                amount: 0,
-                percent: 0
-            },
-            f: {
-                amount: 0,
-                percent: 0
-            }
-        }
+        // const parity = {
+        //     m: {
+        //         amount: this.props.firstCountryData.m.length,
+        //         percent: 0
+        //     },
+        //     f: {
+        //         amount: 0,
+        //         percent: 0
+        //     }
+        // }
         // set percent in terms of amount
-        const setPercent = (gender) => Math.floor((gender.amount / data.length) * 100);
+       
+        // this.setState({
+        //     firstCountry: {
+        //         parity
+        //     }
+        // })
         // map over all people in given data, and of people if man, increment m, else f
-        data.map(people => people.gender === 'M' ? parity.m.amount++ : parity.f.amount++);
+        
         // set percent for men & women
-        parity.m.percent = setPercent(parity.m)
-        parity.f.percent = setPercent(parity.f)
+        
         // then, set parity to state
-        if (countrybin === 0) {
-            this.setState(prevState => {
-                let firstCountry = Object.assign({}, prevState.firstCountry);
-                firstCountry.parity = parity;
-                return { firstCountry };
-              })
-        }
-        if (countrybin === 1) {
-            this.setState(prevState => {
-                let secondCountry = Object.assign({}, prevState.secondCountry);
-                secondCountry.parity = parity;
-                return { secondCountry };
-              })
-        }
+        // if (countrybin === 0) {
+        //     parity.m.percent = setPercent(this.props.firstCountryData.m.length)
+        // parity.f.percent = setPercent(this.props.firstCountryData.m.length)
+        //     this.setState(prevState => {
+        //         let firstCountry = Object.assign({}, prevState.firstCountry);
+        //         firstCountry.parity = parity;
+        //         return { firstCountry };
+        //       })
+        // }
+        // if (countrybin === 1) {
+        //     parity.m.percent = setPercent(this.props.secondCountryData.m.length)
+        // parity.f.percent = setPercent(this.props.secondCountryData.m.length)
+        //     this.setState(prevState => {
+        //         let secondCountry = Object.assign({}, prevState.secondCountry);
+        //         secondCountry.parity = parity;
+        //         return { secondCountry };
+        //       })
+        // }
     }
     render() {
-        return <section className="CompareParity">   
+        return <section className="CompareParity"> 
+
           <ParityTemplate
             country={this.state.firstCountry}
           />
