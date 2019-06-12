7baf2d1026ec70256c61fb39f97c07ce38b0e17f
blyndusk <alexandredelaloydev@gmail.com>
2019-06-12 20:44:16 +0200

    Updates

--- precache-manifest.2bf2cd698e0a245b55968763af9274b3.js
+++ precache-manifest.2bf2cd698e0a245b55968763af9274b3.js
@@ -4,16 +4,16 @@ self.__precacheManifest = [
     "url": "/unknown-genius/static/media/futuraMedium.ee64fb9d.ttf"
   },
   {
-    "revision": "ee196d05d83b42d95c53",
-    "url": "/unknown-genius/static/css/main.80a04766.chunk.css"
+    "revision": "45b2c3daa98edaa60d52",
+    "url": "/unknown-genius/static/css/main.a4d98c68.chunk.css"
   },
   {
     "revision": "9ec0f57ea907e5e4562f",
     "url": "/unknown-genius/static/js/runtime~main.ae075ded.js"
   },
   {
-    "revision": "00704341fef10ab32973",
-    "url": "/unknown-genius/static/js/2.0b91feca.chunk.js"
+    "revision": "be5e4d2059dfee279e78",
+    "url": "/unknown-genius/static/js/2.df409217.chunk.js"
   },
   {
     "revision": "156a7d3b9b57fc5f81e6499fcbf147cb",
@@ -28,32 +28,40 @@ self.__precacheManifest = [
     "url": "/unknown-genius/static/media/timeline-big-active.bfbe9605.svg"
   },
   {
-    "revision": "0ef1f4f180ec68b1aa51a63a8bd2ac08",
-    "url": "/unknown-genius/static/media/compare.0ef1f4f1.svg"
-  },
-  {
     "revision": "8019ec01e2e40d701dae2df66ac4942b",
     "url": "/unknown-genius/static/media/timeline-big.8019ec01.svg"
+  },
+  {
+    "revision": "0ef1f4f180ec68b1aa51a63a8bd2ac08",
+    "url": "/unknown-genius/static/media/compare.0ef1f4f1.svg"
   },
   {
     "revision": "f9f02ed05aa86534c3842d44cb20d6c4",
     "url": "/unknown-genius/static/media/futuraBook.f9f02ed0.ttf"
   },
   {
-    "revision": "ee196d05d83b42d95c53",
-    "url": "/unknown-genius/static/js/main.dbb333f2.chunk.js"
+    "revision": "45b2c3daa98edaa60d52",
+    "url": "/unknown-genius/static/js/main.10247b29.chunk.js"
   },
   {
     "revision": "a06396ecbce905ec8b8fd35ee80ff765",
     "url": "/unknown-genius/static/media/LemonMilk.a06396ec.woff"
   },
   {
+    "revision": "9a1626276b430d216809407c3fbcda75",
+    "url": "/unknown-genius/static/media/futuraBold.9a162627.ttf"
+  },
+  {
     "revision": "ea9f3c8c2ee1b64b1bfd2d0dbd7a2664",
     "url": "/unknown-genius/static/media/LemonMilkBold.ea9f3c8c.woff"
   },
   {
-    "revision": "9a1626276b430d216809407c3fbcda75",
-    "url": "/unknown-genius/static/media/futuraBold.9a162627.ttf"
+    "revision": "49f9490f9497fe21c61d57dabe569e16",
+    "url": "/unknown-genius/static/media/map-big.49f9490f.svg"
+  },
+  {
+    "revision": "2fbaaa96d4c59290e6c7bfedcd49b44e",
+    "url": "/unknown-genius/static/media/map-big-active.2fbaaa96.svg"
   },
   {
     "revision": "7f1591e4afe4d9e302e71e14c8afa0c8",
@@ -64,19 +72,11 @@ self.__precacheManifest = [
     "url": "/unknown-genius/static/media/map-active.378679a8.svg"
   },
   {
-    "revision": "2fbaaa96d4c59290e6c7bfedcd49b44e",
-    "url": "/unknown-genius/static/media/map-big-active.2fbaaa96.svg"
-  },
-  {
-    "revision": "49f9490f9497fe21c61d57dabe569e16",
-    "url": "/unknown-genius/static/media/map-big.49f9490f.svg"
-  },
-  {
     "revision": "64137465409474268948a88ed800448b",
     "url": "/unknown-genius/static/media/picture.64137465.png"
   },
   {
-    "revision": "f9848f953904e204b3b8f1cd9236a941",
+    "revision": "03d44ff88a7fadcfb1d7f568ffecd047",
     "url": "/unknown-genius/index.html"
   }
 ];
