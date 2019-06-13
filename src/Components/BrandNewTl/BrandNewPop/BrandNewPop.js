61c7ee8fdc948dd28df0f069a89975ef4c1c8ff4
blyndusk <alexandredelaloydev@gmail.com>
2019-06-14 01:59:09 +0200

    :speech_balloon: custom pop

--- src/Components/BrandNewTl/BrandNewPop/BrandNewPop.js
+++ src/Components/BrandNewTl/BrandNewPop/BrandNewPop.js
@@ -5,9 +5,17 @@ import { spawn } from 'child_process';
 class BrandNewPop extends Component {
     
     render() {
-        return <div className="BrandNewPop">
-            {Object.keys(this.props.data).map((woman, i) => <span key={i}>{woman}</span>)}
-        </div>
+        console.log(this.props.data)
+        return this.props.data.idcountry ? <div className="BrandNewPop">
+            <h2>{this.props.data.firstname} {this.props.data.name}</h2>
+            <span>{this.props.data.birthday}</span>
+            <span>{this.props.data.deathdate}</span>
+            <span>{this.props.data.idcountry['name']}</span>
+            <ul>
+                {this.props.data.idprice.map((price, i) => <li key={i}>{price.idcategory.category} - {price.year}</li>)}
+            </ul>
+        </div> : null
+        
     }
 }
 
