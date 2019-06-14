11adc66867be74068709fc8976494ed50990b2a7
blyndusk <alexandredelaloydev@gmail.com>
2019-06-14 04:15:45 +0200

    :sparkles: add bg

--- src/Components/BrandNewTl/BrandNewTl.js
+++ src/Components/BrandNewTl/BrandNewTl.js
@@ -70,7 +70,7 @@ class BrandNewTl extends Component {
                     <ul className="BrandNewTl__laureats">
                         {woman.map((li, i) => {
                             return <li className="BrandNewTl__laureat" key={i} onClick={() => this.displayWoman(li)}>
-                                <div className="BrandNewTl__image"></div>
+                                <div className="BrandNewTl__image" style={{background: `url('./women/${li.idpeople}.jpg') no-repeat center/cover`}}></div>
                                 <p className="BrandNewTl__name"><span>{li.firstname}</span><span>{li.name}</span></p>
                             </li>
                         })}
