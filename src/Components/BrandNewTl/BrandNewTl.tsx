b3070dd7e5b56e7ce5d08f985809b9b33f1242b1
blyndusk <alexandredelaloydev@gmail.com>
2019-06-17 14:29:43 +0200

    :rotating_light: fix brand newx tl warns

--- src/Components/BrandNewTl/BrandNewTl.tsx
+++ src/Components/BrandNewTl/BrandNewTl.tsx
@@ -6,43 +6,61 @@ import Compare from './../Compare/Compare'
 import BrandNewPop from './BrandNewPop/BrandNewPop'
 import axios from 'axios';
 
-class BrandNewTl extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            baseUrl: 'http://localhost:8000/api/',
-            // 2 types of API filters
-            type: [
-                'people',
-                'prices', 
-                'categories'
-            ],
-            data: [],
-            dates: [
-                "1901-1920",
-                "1921-1940",
-                "1941-1960",
-                "1961-1980",
-                "1981-2000",
-                "2001-2018",
-            ],
-            woman: {},
-            displayWomen: false,
-            coords: {
-                x: 0,
-                y: 0
+interface BrandNewTlProps {
+
+}
+interface BrandNewTlState {
+    baseUrl: 'http://localhost:8000/api/',
+    // 2 types of API filters
+    type: [
+        'people',
+        'prices', 
+        'categories'
+    ],
+    data: {}[][],
+    dates: [
+        "1901-1920",
+        "1921-1940",
+        "1941-1960",
+        "1961-1980",
+        "1981-2000",
+        "2001-2018",
+    ],
+    woman: {
+        idcountry: {
+            name: string
+        },
+        idpeople: number,
+        firstname: string,
+        name: string,
+        idprice: [
+            {
+                year: number,
+                idcategory: {
+                    category: string
+                }
             }
-        }
+        ],
+        birthday: string,
+        deathdate: string
+    },
+    displayWomen: false,
+    coords: {
+        x: 0,
+        y: 0
     }
+}
+
+class BrandNewTl extends React.Component<BrandNewTlProps, BrandNewTlState> {
     componentDidMount = () => {
         axios.get(this.state.baseUrl + this.state.type[0])
             .then(res => {
                 const response = res.data["hydra:member"];
-                const womanData = [];
-                const data = [[], [], [], [], [], []]
-                response.map(people => people.gender === 'F' ? womanData.push(people) : null)
-                womanData.map(woman => {
-                    const year = woman.idprice[0].year
+                const womanData: any = [];
+                const data: {}[][] = [[], [], [], [], [], []]
+                response.map((people: { gender: string}) => people.gender === 'F' ? womanData.push(people) : null)
+                womanData.map((woman: any) => {
+                    const year: number = woman.idprice[0].year
                     if (year <= 1921 ) data[0].push(woman)
                     else if (year <= 1941 ) data[1].push(woman)
                     else if (year <= 1961 ) data[2].push(woman)
@@ -54,17 +72,19 @@ class BrandNewTl extends Component {
                 this.setState({ data })
             })
     }
-    displayWomen = (e) => {
-        [...document.querySelectorAll('.BrandNewTl__item')].map(child => child.classList.remove('BrandNewTl__item--active'));
-        e.target.parentNode.classList.toggle('BrandNewTl__item--active')
-        // document.querySelector('.BrandNewPop').style.display = 'none'
-        if (document.querySelector('.BrandNewPop')) document.querySelector('.BrandNewPop').style.opacity = 0
-        document.querySelector('.BrandNewTl__tutorial').classList.add('BrandNewTl__tutorial--hidden')
+    displayWomen = (e: { target: HTMLElement}) => {
+        const pop = document.querySelector('.BrandNewPop') as HTMLElement;
+        Array.from(document.querySelectorAll('.BrandNewTl__item')).map(child => child.classList.remove('BrandNewTl__item--active'));
+        const parent = e.target.parentNode as HTMLElement;
+        parent.classList.toggle('BrandNewTl__item--active') 
+        if (pop) pop!.style.opacity = '0'
+        document.querySelector('.BrandNewTl__tutorial')!.classList.add('BrandNewTl__tutorial--hidden')
         
     }
-    displayWoman = (e, li) => {
+    displayWoman = (e: any, li: any) => {
         console.log(e.clientX, e.clientY)
         console.log(li)
+        const pop = document.querySelector('.BrandNewPop') as HTMLElement;
         
         this.setState({
             woman: li,
@@ -73,7 +93,7 @@ class BrandNewTl extends Component {
                 y: e.clientY > Math.floor(window.innerHeight * 1) ? Math.floor(window.innerHeight * 0.33) : e.clientY + 10
             }
         });
-        if (document.querySelector('.BrandNewPop')) document.querySelector('.BrandNewPop').style.opacity = 1
+        if (pop) pop.style.opacity = '1';
     }
     render() {
         return <section className="BrandNewTl">
@@ -81,15 +101,14 @@ class BrandNewTl extends Component {
             <Nav />
             <p className="BrandNewTl__context">We decided to focus on women, which are winning more and more prizes every year</p>
             <div className="BrandNewTl__items">
-                {this.state.data.map((woman, i) => <div className="BrandNewTl__item" key={i}>
-                    <div className="BrandNewTl__number" onClick={this.displayWomen}>{woman.length}</div>
-                    <div className="BrandNewTl__dates" onClick={this.displayWomen}>{this.state.dates[i]}</div>
+                {this.state.data.map((woman: any, i: number) => <div className="BrandNewTl__item" key={i}>
+                    <div className="BrandNewTl__number" onClick={(e: any) => this.displayWomen(e)}>{woman.length}</div>
+                    <div className="BrandNewTl__dates" onClick={(e: any) => this.displayWomen(e)}>{this.state.dates[i]}</div>
                     <ul className="BrandNewTl__laureats">
-                        {woman.map((li, i) => {
-                            return <li className="BrandNewTl__laureat" key={i} onClick={(e) => this.displayWoman(e, li)}>
+                        {woman.map((li: { idpeople: number }, i: number) => <li className="BrandNewTl__laureat" key={i} onClick={(e) => this.displayWoman(e, li)}>
                                 <div className="BrandNewTl__image" style={{background: `url('./women/${li.idpeople}.jpg') no-repeat center/cover`}}></div>
                             </li>
-                        })}
+                        )}
                     </ul>
                 </div>)}
                 <BrandNewPop
