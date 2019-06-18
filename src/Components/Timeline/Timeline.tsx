aabaf3e9fd6dc64d6d898cb743d8d50bb972ca8d
blyndusk <alexandredelaloydev@gmail.com>
2019-06-18 18:08:11 +0200

    :rotating_light: fix tslint timeline warns

--- src/Components/Timeline/Timeline.tsx
+++ src/Components/Timeline/Timeline.tsx
@@ -1,21 +1,21 @@
 
-import React from 'react';
-import Nav from '../Nav/Nav'
-import Brand from '../Brand/Brand'
-import Compare from '../Compare/Compare'
-import TimelinePop from './TimelinePop/TimelinePop'
 import axios from 'axios';
+import React from 'react';
+import Brand from '../Brand/Brand';
+import Compare from '../Compare/Compare';
+import Nav from '../Nav/Nav';
+import TimelinePop from './TimelinePop/TimelinePop';
 
 interface P {}
 interface S {
     baseUrl: string;
     // 2 types of API filters
     type: string[];
-    data: {}[][],
+    data: Array<Array<{}>>;
     dates: string[];
     woman: {
         idcountry: {
-            name: string
+            name: string,
         },
         idpeople: number,
         firstname: string,
@@ -24,18 +24,18 @@ interface S {
             {
                 year: number,
                 idcategory: {
-                    category: string
-                }
+                    category: string,
+                },
             }
         ],
         birthday: string,
-        deathdate: string
+        deathdate: string,
     };
     displayWomen: boolean;
     coords: {
         x: number,
-        y: number
-    }
+        y: number,
+    };
 }
 
 export default class Timeline extends React.Component<P, S> {
@@ -43,98 +43,99 @@ export default class Timeline extends React.Component<P, S> {
         super(props);
         this.state = {
             baseUrl: 'http://localhost:8000/api/',
+            coords: { x: 0, y: 0 },
+            data: [],
+            dates: [
+                '1901-1920',
+                '1921-1940',
+                '1941-1960',
+                '1961-1980',
+                '1981-2000',
+                '2001-2018',
+            ],
+            displayWomen: false,
+            woman: {
+                birthday: '',
+                deathdate: '',
+                firstname: '',
+                idcountry: {
+                    name: '',
+                },
+                idpeople: 0,
+                idprice: [
+                    {
+                        idcategory: {
+                            category: '',
+                        },
+                        year: 0,
+                    },
+                ],
+                name: '',
+            },
+
             // 2 types of API filters
             type: [
                 'people',
-                'prices', 
-                'categories'
+                'prices',
+                'categories',
             ],
-            data: [],
-            dates: [
-                "1901-1920",
-                "1921-1940",
-                "1941-1960",
-                "1961-1980",
-                "1981-2000",
-                "2001-2018",
-            ],
-            woman: {
-                idcountry: {
-                    name: ''
-                },
-                idpeople: 0,
-                firstname: '',
-                name: '',
-                idprice: [
-                    {
-                        year: 0,
-                        idcategory: {
-                            category: ''
-                        }
-                    }
-                ],
-                birthday: '',
-                deathdate: ''
-            },
-            displayWomen: false,
-            coords: { x: 0, y: 0 }
-        }
+        };
     }
     componentDidMount = () => {
         axios.get(this.state.baseUrl + this.state.type[0])
             .then(res => {
-                const response = res.data["hydra:member"];
+                const response = res.data['hydra:member'];
                 const womanData: any = [];
-                const data: {}[][] = [[], [], [], [], [], []]
-                response.map((people: { gender: string}) => people.gender === 'F' ? womanData.push(people) : null)
+                const data: Array<Array<{}>> = [[], [], [], [], [], []];
+                response.map((people: { gender: string}) => people.gender === 'F' ? womanData.push(people) : null);
                 womanData.map((woman: any) => {
-                    const year: number = woman.idprice[0].year
-                    if (year <= 1921 ) data[0].push(woman)
-                    else if (year <= 1941 ) data[1].push(woman)
-                    else if (year <= 1961 ) data[2].push(woman)
-                    else if (year <= 1981 ) data[3].push(woman)
-                    else if (year <= 2001 ) data[4].push(woman)
-                    else if (year <= 2020 ) data[5].push(woman)
+                    const year: number = woman.idprice[0].year;
+                    if (year <= 1921) data[0].push(woman);
+                    else if (year <= 1941) data[1].push(woman);
+                    else if (year <= 1961) data[2].push(woman);
+                    else if (year <= 1981) data[3].push(woman);
+                    else if (year <= 2001) data[4].push(woman);
+                    else if (year <= 2020) data[5].push(woman);
                     return woman;
-                })
-                this.setState({ data })
-            })
+                });
+                this.setState({ data });
+            });
     }
     displayWomen = (e: { target: HTMLElement}) => {
         const pop = document.querySelector('.BrandNewPop') as HTMLElement;
         Array.from(document.querySelectorAll('.BrandNewTl__item')).map(child => child.classList.remove('BrandNewTl__item--active'));
         const parent = e.target.parentNode as HTMLElement;
-        parent.classList.toggle('BrandNewTl__item--active') 
-        if (pop) pop!.style.opacity = '0'
-        document.querySelector('.BrandNewTl__tutorial')!.classList.add('BrandNewTl__tutorial--hidden')
+        parent.classList.toggle('BrandNewTl__item--active');
+        if (pop) pop!.style.opacity = '0';
+        document.querySelector('.BrandNewTl__tutorial')!.classList.add('BrandNewTl__tutorial--hidden');
     }
     displayWoman = (e: any, li: any) => {
-        console.log(e.clientX, e.clientY)
-        console.log(li)
         const pop = document.querySelector('.BrandNewPop') as HTMLElement;
-        
         this.setState({
-            woman: li,
             coords: {
                 x: e.clientX > Math.floor(window.innerWidth * 1) ? Math.floor(window.innerWidth * 0.66) : e.clientX + 10,
-                y: e.clientY > Math.floor(window.innerHeight * 1) ? Math.floor(window.innerHeight * 0.33) : e.clientY + 10
-            }
+                y: e.clientY > Math.floor(window.innerHeight * 1) ? Math.floor(window.innerHeight * 0.33) : e.clientY + 10,
+            },
+            woman: li,
         });
         if (pop) pop.style.opacity = '1';
     }
     render() {
-        return <section className="BrandNewTl">
+        return <section className='BrandNewTl'>
             <Brand />
             <Nav />
-            <p className="BrandNewTl__context">We decided to focus on women, which are winning more and more prizes every year</p>
-            <div className="BrandNewTl__items">
-                {this.state.data.map((woman: any, i: number) => <div className="BrandNewTl__item" key={i}>
-                    <div className="BrandNewTl__number" onClick={(e: any) => this.displayWomen(e)}>{woman.length}</div>
-                    <div className="BrandNewTl__dates" onClick={(e: any) => this.displayWomen(e)}>{this.state.dates[i]}</div>
-                    <ul className="BrandNewTl__laureats">
-                        {woman.map((li: { idpeople: number }, i: number) => <li className="BrandNewTl__laureat" key={i} onClick={(e) => this.displayWoman(e, li)}>
-                                <div className="BrandNewTl__image" style={{background: `url('./women/${li.idpeople}.jpg') no-repeat center/cover`}}></div>
-                            </li>
+            <p className='BrandNewTl__context'>We decided to focus on women, which are winning more and more prizes every year</p>
+            <div className='BrandNewTl__items'>
+                {this.state.data.map((woman: any, i: number) => <div className='BrandNewTl__item' key={i}>
+                    <div className='BrandNewTl__number' onClick={(e: any) => this.displayWomen(e)}>{woman.length}</div>
+                    <div className='BrandNewTl__dates' onClick={(e: any) => this.displayWomen(e)}>{this.state.dates[i]}</div>
+                    <ul className='BrandNewTl__laureats'>
+                        {woman.map((li: { idpeople: number }, y: number) => <li className='BrandNewTl__laureat' key={y} onClick={e => this.displayWoman(e, li)}>
+                                <div
+                                    className='BrandNewTl__image'
+                                    style={{background: `url('./women/${li.idpeople}.jpg') no-repeat center/cover`}}
+                                />
+                            </li>,
                         )}
                     </ul>
                 </div>)}
@@ -143,8 +144,8 @@ export default class Timeline extends React.Component<P, S> {
                     coords={this.state.coords}
                 />
             </div>
-            <p className="BrandNewTl__tutorial">Select colored period to visualize laureats</p>
+            <p className='BrandNewTl__tutorial'>Select colored period to visualize laureats</p>
             <Compare />
-        </section>
+        </section>;
     }
 }
--- src/Components/Timeline/TimelinePop/TimelinePop.tsx
+++ src/Components/Timeline/TimelinePop/TimelinePop.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 interface P {
     data: {
         idcountry: {
-            name: string
+            name: string,
         },
         idpeople: number,
         firstname: string,
@@ -12,17 +12,17 @@ interface P {
             {
                 year: number,
                 idcategory: {
-                    category: string
-                }
+                    category: string,
+                },
             }
         ],
         birthday: string,
-        deathdate: string
-    },
-    coords: { x: number, y: number }
+        deathdate: string,
+    };
+    coords: { x: number, y: number };
 }
 interface S {
-    months: string[][]
+    months: string[][];
 }
 
 export default class TimelinePop extends React.Component<P, S>  {
@@ -32,46 +32,56 @@ export default class TimelinePop extends React.Component<P, S>  {
             months: [
                 ['01', 'January'], ['02', 'February'], ['03', 'March'],
                 ['04', 'April'], ['05', 'May'], ['06', 'June'],
-                ['07', 'July'], ['08', 'August'], ['09', 'September'], 
-                ['10', 'October'], ['11', 'November'], ['12', 'December']
-            ]
-        }
+                ['07', 'July'], ['08', 'August'], ['09', 'September'],
+                ['10', 'October'], ['11', 'November'], ['12', 'December'],
+            ],
+        };
     }
     setDate = (date: string) => {
         const newDate = date.replace(/T\w+|:\w+|\+\w+/g, '').replace(/-/g, ',').split(',').reverse();
-        this.state.months.map((month) => newDate[1] === month[0] ? newDate.splice(1, 1, month[1]) : null)
-        return newDate.join(' ')
+        this.state.months.map(month => newDate[1] === month[0] ? newDate.splice(1, 1, month[1]) : null);
+        return newDate.join(' ');
     }
     render() {
-        console.log(this.props.data)
-        return this.props.data.idcountry.name.length ? <div className="BrandNewPop" style={{
-            top: `${this.props.coords.y + 10}px`,
-            left: `${this.props.coords.x + 10}px`,
-        }}>
-            <div className="BrandNewPop__image" style={{background: `url('./women/${this.props.data.idpeople}.jpg') no-repeat center/cover`}}></div>
-            <h2 className="BrandNewPop__name">{this.props.data.firstname} {this.props.data.name}</h2>
-            <ul className="BrandNewPop__prizes">
-                {this.props.data.idprice.map((price: any, i: number) => <li className="BrandNewPop__prize" key={i}>{price.idcategory.category} - {price.year}</li>)}
+        return this.props.data.idcountry.name.length ? <div
+            className='BrandNewPop'
+            style={{
+                left: `${this.props.coords.x + 10}px`,
+                top: `${this.props.coords.y + 10}px`,
+            }}
+        >
+            <div
+                className='BrandNewPop__image'
+                style={{background: `url('./women/${this.props.data.idpeople}.jpg') no-repeat center/cover`}}
+            />
+            <h2 className='BrandNewPop__name'>{this.props.data.firstname} {this.props.data.name}</h2>
+            <ul className='BrandNewPop__prizes'>
+                {this.props.data.idprice.map((price: any, i: number) => <li
+                    className='BrandNewPop__prize'
+                    key={i}
+                >
+                    {price.idcategory.category} - {price.year}
+                </li>)}
             </ul>
-              
+
             {this.props.data.birthday ?
-                <div className="BrandNewPop__section">
-                    <h3 className="BrandNewPop__subtitle">Born :</h3>
-                    <span className="BrandNewPop__text">{this.setDate(this.props.data.birthday)}</span>
+                <div className='BrandNewPop__section'>
+                    <h3 className='BrandNewPop__subtitle'>Born :</h3>
+                    <span className='BrandNewPop__text'>{this.setDate(this.props.data.birthday)}</span>
                 </div>
             : null}
-             
+
             {this.props.data.deathdate ?
-                <div className="BrandNewPop__section">
-                    <h3 className="BrandNewPop__subtitle">Died :</h3>
-                    <span className="BrandNewPop__text">{this.setDate(this.props.data.deathdate)}</span>
+                <div className='BrandNewPop__section'>
+                    <h3 className='BrandNewPop__subtitle'>Died :</h3>
+                    <span className='BrandNewPop__text'>{this.setDate(this.props.data.deathdate)}</span>
                 </div>
             : null}
-            
-            <div className="BrandNewPop__section">
-                <h3 className="BrandNewPop__subtitle">Nationality :</h3>
-                <span className="BrandNewPop__text">{this.props.data.idcountry['name']}</span>
+
+            <div className='BrandNewPop__section'>
+                <h3 className='BrandNewPop__subtitle'>Nationality :</h3>
+                <span className='BrandNewPop__text'>{this.props.data.idcountry.name}</span>
             </div>
-        </div> : null
+        </div> : null;
     }
 }
