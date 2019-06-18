67de63479709ded1235c343fd6a87a4f87c44917
blyndusk <alexandredelaloydev@gmail.com>
2019-06-18 20:15:14 +0200

    Updates

---
+++ precache-manifest.59cb2e7306104606341487043b6aa7dd.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eebcb513b3b482744cff8b4db1f12659",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b383fcbb5aaa2ba8a66b",
+    "url": "/static/css/main.5dd2f3fa.chunk.css"
+  },
+  {
+    "revision": "eadd7c876aee2f9a96ee",
+    "url": "/static/js/2.a1c424c0.chunk.js"
+  },
+  {
+    "revision": "b383fcbb5aaa2ba8a66b",
+    "url": "/static/js/main.93216ba8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a06396ecbce905ec8b8fd35ee80ff765",
+    "url": "/static/media/LemonMilk.a06396ec.woff"
+  },
+  {
+    "revision": "ea9f3c8c2ee1b64b1bfd2d0dbd7a2664",
+    "url": "/static/media/LemonMilkBold.ea9f3c8c.woff"
+  },
+  {
+    "revision": "d4408b9c7d94a6ee0fefde38fa00180a",
+    "url": "/static/media/arrow.d4408b9c.svg"
+  },
+  {
+    "revision": "0ef1f4f180ec68b1aa51a63a8bd2ac08",
+    "url": "/static/media/compare.0ef1f4f1.svg"
+  },
+  {
+    "revision": "12c7115565d05cfa22e7b1a9686de7fb",
+    "url": "/static/media/cross-active.12c71155.svg"
+  },
+  {
+    "revision": "cdfca5de2724a248e0bc756cbeb7442b",
+    "url": "/static/media/cross.cdfca5de.svg"
+  },
+  {
+    "revision": "9a1626276b430d216809407c3fbcda75",
+    "url": "/static/media/futuraBold.9a162627.ttf"
+  },
+  {
+    "revision": "f9f02ed05aa86534c3842d44cb20d6c4",
+    "url": "/static/media/futuraBook.f9f02ed0.ttf"
+  },
+  {
+    "revision": "ee64fb9d3f1ba2333e1b489283925bce",
+    "url": "/static/media/futuraMedium.ee64fb9d.ttf"
+  },
+  {
+    "revision": "378679a8e8f79a6f4260d3a1c2a7d3bc",
+    "url": "/static/media/map-active.378679a8.svg"
+  },
+  {
+    "revision": "2fbaaa96d4c59290e6c7bfedcd49b44e",
+    "url": "/static/media/map-big-active.2fbaaa96.svg"
+  },
+  {
+    "revision": "49f9490f9497fe21c61d57dabe569e16",
+    "url": "/static/media/map-big.49f9490f.svg"
+  },
+  {
+    "revision": "7f1591e4afe4d9e302e71e14c8afa0c8",
+    "url": "/static/media/map.7f1591e4.svg"
+  },
+  {
+    "revision": "64137465409474268948a88ed800448b",
+    "url": "/static/media/picture.64137465.png"
+  },
+  {
+    "revision": "156a7d3b9b57fc5f81e6499fcbf147cb",
+    "url": "/static/media/timeline-active.156a7d3b.svg"
+  },
+  {
+    "revision": "bfbe9605855348b3bd2dd8d3dd64ea43",
+    "url": "/static/media/timeline-big-active.bfbe9605.svg"
+  },
+  {
+    "revision": "8019ec01e2e40d701dae2df66ac4942b",
+    "url": "/static/media/timeline-big.8019ec01.svg"
+  },
+  {
+    "revision": "1e65e093044ae6c19f0bb0901e14a29d",
+    "url": "/static/media/timeline.1e65e093.svg"
+  }
+]);
